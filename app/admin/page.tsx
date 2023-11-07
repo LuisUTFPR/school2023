eb3958a01e0ebd07112bc0dbb54991a2d0eafb04
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-11-07 23:40:11 +0000

    fet: lista de cursos

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function Admin(){
+    return (
+        <div>
+        <h1 className="text-center text-xl">ADM</h1>
+        <a href= "/admin/course"> Listar Curso </a>
+        </div>
+
+    )
+}
