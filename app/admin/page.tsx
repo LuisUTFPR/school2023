4e43c0375d3457cea6ebb4fd92bb6aafc59bf5a9
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-11-08 00:41:39 +0000

    nome

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ export default function Admin(){
     return (
         <div>
         <h1 className="text-center text-xl">ADM</h1>
-        <a href= "/admin/course"> Listar Curso </a>
+        <a href= "/admin/course"> Listar Curso I'm  </a>
         </div>
 
     )
